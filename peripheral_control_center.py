c05a3dfded03428a83c161de0a0b56b65c154c2f
kpmmmurphy <kpmmmurphy@gmail.com>
2015-02-28 15:37:41 +0000

    Adding socket code

--- peripheral_control_center.py
+++ peripheral_control_center.py
@@ -6,6 +6,8 @@
 
 import json
 import time
+import socket
+import threading 
 import pyupm_grove as grove
 import pyupm_buzzer as upmBuzzer
 import pyupm_ttp223 as ttp223
@@ -24,20 +26,63 @@ buzzer = None
 touch  = None
 led    = None
 
+#SOCKET
+MULTICAST_GRP  = '224.1.1.1'
+MULTICAST_PORT = 5007
+DEFAULT_PORT   = 5006
+DEFAULT_SERVER_PORT = 5005
+
 #Buzzer Notes
 chords = [upmBuzzer.DO, upmBuzzer.RE, upmBuzzer.MI, upmBuzzer.FA, 
           upmBuzzer.SOL, upmBuzzer.LA, upmBuzzer.SI, upmBuzzer.DO, 
           upmBuzzer.SI]
 
 def main():
+	sensorReadings = {}
 	createSensors()
+	ipAddress = getIPAddress()
+	print "ipAddress", ipAddress
+	multicastSocket = self.createMulticatSocket(ipAddress, CONSTS.MULTICAST_GRP, CONSTS.MULTICAST_PORT)
 	while True:
-		print checkTouchPressed(touch)
-		print readTemperature(temp)
-		print readLightLevel(light)
-		soundBuzzer(buzzer)
-		flashLed(led)
+		sensorReadings[touch.name()] = checkTouchPressed(touch)
+		sensorReadings[temp.name()]  = readTemperature(temp)
+		sensorReadings[led.name()]   = readLightLevel(light)
+		print json.dumps(sensorReadings)
 
+
+
+#SOCKET STUFF
+def getIPAddress():
+	return socket.gethostbyname(socket.gethostname())
+def createSocket(bindToIP, connectToIP):
+	newSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	newSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	newSocket.setblocking(True)
+
+	if bindToIP is not None:
+		#For receiving 
+		newSocket.bind((bindToIP, CONSTS.DEFAULT_SERVER_PORT))
+		newSocket.listen(5)
+	elif connectToIP is not None:
+		#For sending
+		#newSocket.bind((self.__ipAddress, CONSTS.DEFAULT_PORT))
+		newSocket.connect((connectToIP, CONSTS.DEFAULT_PORT))
+
+	return newSocket
+
+def createMulticatSocket(inetIP, multicastGroup, multicastPort):
+		multicastSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+		multicastSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		#Socket must be connected to the wlan0 interface's IP address
+		#Bind to our default Multicast Port.
+		multicastSocket.bind((multicastGroup, multicastPort))
+		#multicastSocket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(multicastGroup)+socket.inet_aton(inetIP))
+		multicastSocket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP)
+		multicastSocket.setblocking(True)	
+		return multicastSocket
+
+
+#SENSORS----------------------
 def createSensors():
 	global temp, light, buzzer, touch, led
 
